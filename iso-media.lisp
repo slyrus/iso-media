4aed1bdb3b81280176d9c052b6263d74042dfdcc
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-12-20 21:48:44 -0800

    rework find-box-class
     * use a gf based on (eql '<symbol>) methods instead of maintaing our
       own class hash to map from box type to class
    
     * add some comments
    
     * rework copyright symbol/string machinery
    
     * clean up itunes-getter functions a bit

--- iso-media.lisp
+++ iso-media.lisp
@@ -1,35 +1,45 @@
 
-;;
-;; reading ISO media files
-;; spec can be found here: http://standards.iso.org/ittf/PubliclyAvailableStandards/c041828_ISO_IEC_14496-12_2005(E).zip
-;;
+;;;
+;;; A package for reading and writing ISO (MP4) media files
+;;; by Cyrus Harmon <ch-lisp@bobobeach.com>
+;;;
+;;; The ISO Media spec can be found at:
+;;; <http://standards.iso.org/ittf/PubliclyAvailableStandards/c041828_ISO_IEC_14496-12_2005(E).zip>
+;;;
+;;; ISO Media files are composed of boxes, which can contain other
+;;; boxes. The boxes all begin with a type and size; specific box
+;;; types add additional fields and then generally contain either more
+;;; boxes, in the case of what I call container-boxes, or a big hunk
+;;; of encoded media, which I call data-boxes.
+;;;
 
 (cl:in-package #:iso-media)
- 
+
 (defgeneric find-child (node type))
 (defgeneric filter-children (node type))
 
+;;
+;; ISO file headers use a (32-bit) box size of 1 as a sentinel value
+;; to indicate that the true value of the box size is stored in a
+;; 64-bit large-size field.
 (defun large-size-p (size) (= size 1))
 
+;;
+;; FIXME: Add support for size 0 boxes, which aren't really size 0,
+;; but indicate that the contents run through to the end of the file!
+
+;;
+;; Use Peter Seibel's binary-data package to represent boxes. The
+;; binary-data package can be found at:
+;;  <https://github.com/gigamonkey/monkeylib-binary-data>
+;;
 (define-tagged-binary-class bbox ()
   ((size u4)
    (box-type (iso-8859-1-string :length 4))
    (large-size (optional :type 'u8 :if (large-size-p size)))
    (user-type (optional :type '(raw-bytes :size 16)
                         :if (equal box-type "uuid"))))
-  (:dispatch (find-box-class box-type)))
-
-(defgeneric header-size (object)
-  (:method-combination +))
-
-(defmethod header-size + ((obj bbox))
-  (with-slots (large-size user-type) obj
-    (+ 8
-       (if large-size 4 0)
-       (if user-type 16 0))))
-
-(defmethod data-size ((obj bbox))
-  (- (size obj) (header-size obj)))
+  (:dispatch (find-box-class (intern box-type (load-time-value *package*)))))
 
 (defmethod print-object ((object bbox) stream)
   (print-unreadable-object (object stream :type t)
@@ -37,6 +47,39 @@
                  (box-type box-type)) object
       (format stream "~s :size ~d" box-type size))))
 
+;;
+;; Determining the size of the box header is a bit tricky. There are
+;; multiple factors that influence the size of the box header. First,
+;; all boxes have a size and type, but both the size and type can be
+;; extended to a large size, in the case of the size, and a user type
+;; in the case of a type. We need to know the size of the box header
+;; in order to read in the box data, but we need to know the type of
+;; the box (and the value of some of the header fields) in order to
+;; compute the size of the box and its header. So, we use a generic
+;; function with the #'+ method combination such that the value
+;; returned upon calling header-size for a given object will be the
+;; sum of the values returned from each header-size method for the
+;; class and its superclasses.
+(defgeneric header-size (object)
+  (:method-combination +))
+
+;; All boxes have the following header, mentioned above, which contain
+;; the type and size of the box, along with the large-size and the
+;; user-type, if necessary.
+(defmethod header-size + ((obj bbox))
+  (with-slots (large-size user-type) obj
+    (+ 8
+       (if large-size 4 0)
+       (if user-type 16 0))))
+
+;; For our default (non-container) box, the size of the actual data
+;; for the box will be the size of the box minus the size of the
+;; header.
+(defmethod data-size ((obj bbox))
+  (- (size obj) (header-size obj)))
+
+;; The spec defines a "full box" which contains additionl fields for a
+;; version and various flags. 
 (define-binary-class full-bbox-header (bbox)
   ((version u1)
    (flags u3)))
@@ -215,59 +258,57 @@
 
 (defmethod header-size + ((obj apple-data-bbox)) 4)
 
-(defparameter *copyright-symbol-string* #.(string (code-char 169)))
+(defparameter *copyright-symbol-string* (string (code-char 169)))
 
-(defparameter *bbox-type-hash* (make-hash-table :test 'equal))
-(progn 
-  (clrhash *bbox-type-hash*)
-  (map nil (lambda (x)
-             (destructuring-bind (type class) x
-               (setf (gethash type *bbox-type-hash*) class)))
-       `(("moov" container-bbox)
-         ("trak" container-bbox)
-         ("mdia" container-bbox)
-         ("minf" container-bbox)
-         ("stbl" container-bbox)
-         ("udta" container-bbox)
-         ("meta" meta-bbox)
-         ("hdlr" handler-bbox)
-         ("mdat" movie-data-bbox)
-         ("stsd" sample-description-bbox)
-         ("mvhd" movie-header-bbox)
-         ("tkhd" track-header-bbox)
-         ("mdhd" media-header-bbox)
+(defun make-copyright-symbol-string (suffix)
+  (concatenate 'string *copyright-symbol-string* suffix))
 
-         ("ilst" container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "nam") container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "ART") container-bbox)
-         ("aART" container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "alb") container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "grp") container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "day") container-bbox)
-         ("trkn" container-bbox)
-         ("disk" container-bbox)
-         ("tmpo" container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "wrt") container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "cmt") container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "gen") container-bbox)
-         ("gnre" container-bbox)
-         ("cpil" container-bbox)
-         ("tvsh" container-bbox)
-         ("sonm" container-bbox)
-         ("soar" container-bbox)
-         ("soaa" container-bbox)
-         ("soal" container-bbox)
-         ("soco" container-bbox)
-         ("sosn" container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "lyr") container-bbox)
-         ("covr" container-bbox)
-         (,(concatenate 'string *copyright-symbol-string* "too") container-bbox)
-         
-         ("data" apple-data-bbox))))
+(defun make-copyright-symbol-symbol (string)
+  (intern (make-copyright-symbol-string string)
+          (load-time-value *package*)))
 
-(defun find-box-class (box-type)
-  (or (gethash box-type *bbox-type-hash*)
-      'data-bbox))
+(defgeneric find-box-class (box-type)
+  (:method (box-type) 'data-bbox)
+  (:method ((box-type (eql '|moov|))) 'container-bbox)
+  (:method ((box-type (eql '|trak|))) 'container-bbox)
+  (:method ((box-type (eql '|mdia|))) 'container-bbox)
+  (:method ((box-type (eql '|minf|))) 'container-bbox)
+  (:method ((box-type (eql '|stbl|))) 'container-bbox)
+  (:method ((box-type (eql '|udta|))) 'container-bbox)
+  (:method ((box-type (eql '|meta|))) 'meta-bbox)
+  (:method ((box-type (eql '|hdlr|))) 'handler-bbox)
+  (:method ((box-type (eql '|mdat|))) 'movie-data-bbox)
+  (:method ((box-type (eql '|stsd|))) 'sample-description-bbox)
+  (:method ((box-type (eql '|mvhd|))) 'movie-header-bbox)
+  (:method ((box-type (eql '|tkhd|))) 'track-header-bbox)
+  (:method ((box-type (eql '|mdhd|))) 'media-header-bbox)
+  (:method ((box-type (eql '|data|))) 'apple-data-bbox)
+  (:method ((box-type (eql '|ilst|))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "nam")))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "ART")))) 'container-bbox)
+  (:method ((box-type (eql '|aART|))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "alb")))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "grp")))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "day")))) 'container-bbox)
+  (:method ((box-type (eql '|trkn|))) 'container-bbox)
+  (:method ((box-type (eql '|disk|))) 'container-bbox)
+  (:method ((box-type (eql '|tmpo|))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "wrt")))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "cmt")))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "gen")))) 'container-bbox)
+  (:method ((box-type (eql '|gnre|))) 'container-bbox)
+  (:method ((box-type (eql '|cpil|))) 'container-bbox)
+  (:method ((box-type (eql '|tvsh|))) 'container-bbox)
+  (:method ((box-type (eql '|sonm|))) 'container-bbox)
+  (:method ((box-type (eql '|soar|))) 'container-bbox)
+  (:method ((box-type (eql '|soaa|))) 'container-bbox)
+  (:method ((box-type (eql '|soal|))) 'container-bbox)
+  (:method ((box-type (eql '|soco|))) 'container-bbox)
+  (:method ((box-type (eql '|sosn|))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "lyr")))) 'container-bbox)
+  (:method ((box-type (eql '|covr|))) 'container-bbox)
+  (:method ((box-type (eql (make-copyright-symbol-symbol "too")))) 'container-bbox))
+
 
 ;;;
 ;;; functions to access data in iso-containers
@@ -280,28 +321,36 @@
               iso-container
               "moov" "trak" "mdia" "minf" "stbl" "stsd"))))))
 
+(defun itunes-container-box (iso-container type)
+  (reduce #'(lambda (x y) (when x (find-child x y)))
+          (list iso-container "moov" "udta" "meta" "ilst" type "data")))
+
 (defun itunes-container-box-info (iso-container type)
-  (data
-   (reduce #'find-child
-           (list iso-container "moov" "udta" "meta" "ilst" type "data"))))
+  (let ((box (itunes-container-box iso-container type)))
+    (print (flags box))
+    (when box (data box))))
 
 (macrolet 
     ((defitunes-getter (accessor-name accessor-type)
        `(defun ,accessor-name (iso-container)
-          (map 'string #'code-char
-               (itunes-container-box-info iso-container ,accessor-type)))))
-  (defitunes-getter track-name (concatenate 'string *copyright-symbol-string* "nam"))
-  (defitunes-getter artist (concatenate 'string *copyright-symbol-string* "ART"))
+          (let ((box (itunes-container-box iso-container ,accessor-type)))
+            (when box
+              (cond ((= (flags box) 1)
+                     (map 'string #'code-char
+                          (data box)))
+                    (t (data box))))))))
+  (defitunes-getter track-name (make-copyright-symbol-string "nam"))
+  (defitunes-getter artist (make-copyright-symbol-string "ART"))
   (defitunes-getter album-artist "aART")
-  (defitunes-getter album-name (concatenate 'string *copyright-symbol-string* "alb"))
-  (defitunes-getter grouping (concatenate 'string *copyright-symbol-string* "grp"))
-  (defitunes-getter year-of-publication (concatenate 'string *copyright-symbol-string* "day"))
+  (defitunes-getter album-name (make-copyright-symbol-string "alb"))
+  (defitunes-getter grouping (make-copyright-symbol-string "grp"))
+  (defitunes-getter year-of-publication (make-copyright-symbol-string "day"))
   (defitunes-getter track-number "trkn")
   (defitunes-getter disk-number "disk")
   (defitunes-getter tempo "tmpo")
-  (defitunes-getter composer-name (concatenate 'string *copyright-symbol-string* "wrt"))
-  (defitunes-getter comments (concatenate 'string *copyright-symbol-string* "cmt"))
-  (defitunes-getter genre (concatenate 'string *copyright-symbol-string* "gen"))
+  (defitunes-getter composer-name (make-copyright-symbol-string "wrt"))
+  (defitunes-getter comments (make-copyright-symbol-string "cmt"))
+  (defitunes-getter genre (make-copyright-symbol-string "gen"))
   (defitunes-getter genre-code "gnre")
   (defitunes-getter compilation-part "cpil")
   (defitunes-getter show-name "tvsh")
@@ -311,9 +360,9 @@
   (defitunes-getter sort-album-name "soal")
   (defitunes-getter sort-composer-name "soco")
   (defitunes-getter sort-show-name "sosn")
-  (defitunes-getter lyrics (concatenate 'string *copyright-symbol-string* "lyr"))
+  (defitunes-getter lyrics (make-copyright-symbol-string "lyr"))
   (defitunes-getter cover "covr")
-  (defitunes-getter information (concatenate 'string *copyright-symbol-string* "too")))
+  (defitunes-getter information (make-copyright-symbol-string "too")))
 
 ;;;
 ;;; main read and write routines
